f555d1f304ec29c90d0e51d23f848ea6664ae6b3
Kishu98 <kishujain.1998@gmail.com>
2020-05-29 19:47:37 +0530

    removed key

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -37,5 +37,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-    apiKey: '{AIzaSyAFsqCStMd_oXpzU50Sf_ahzyxMPOP24vk}'
+    apiKey: '{process.env.REACT_APP_MAP_KEY}'
 })(MapContainer);
